b475aa348752a7ce423dfcebd63ac7af01dcd316
EJ <34107946+EJyoungc@users.noreply.github.com>
2025-03-09 05:25:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
+import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import os
 
 is_windows = os.name == 'nt'
 
+# Windows-specific configurations
 libraries = ['rocksdb', 'shlwapi', 'rpcrt4'] if is_windows else ['rocksdb']
-extra_compile_args = ['/std:c++17', '/EHsc', '/D_WIN32_WINNT=0x0601'] if is_windows else ['-std=c++17']
+compile_args = ['/std:c++17', '/EHsc', '/D_WIN32_WINNT=0x0601'] if is_windows else ['-std=c++17']
 
-rocksdb_extension = Extension(
+ext = Extension(
     'rocksdb._rocksdb',
     sources=['rocksdb/_rocksdb.pyx'],
     include_dirs=[
-        os.getenv('INCLUDE_PATH', ''),
-        os.path.join(os.getenv('CONDA_PREFIX', ''), 'Library/include')
+        os.path.join(os.environ.get('CONDA_PREFIX', ''), 'Library/include')
     ],
     library_dirs=[
-        os.getenv('LIB_PATH', ''),
-        os.path.join(os.getenv('CONDA_PREFIX', ''), 'Library/lib')
+        os.path.join(os.environ.get('CONDA_PREFIX', ''), 'Library/lib')
     ],
     libraries=libraries,
     language='c++',
-    extra_compile_args=extra_compile_args,
-    # Windows-specific linker flags
+    extra_compile_args=compile_args,
     extra_link_args=['/MANIFEST'] if is_windows else []
 )
 
 setup(
+    name='python-rocksdb',
+    version='0.9.0',
+    packages=['rocksdb'],
     ext_modules=cythonize(
-        [rocksdb_extension],
+        [ext],
         compiler_directives={'language_level': "3"},
-        force=True  # Ensure rebuild when environment changes
+        force=True
     ),
-    # ... rest of your setup config
+    # ... other metadata
 )
